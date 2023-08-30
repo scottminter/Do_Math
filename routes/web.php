2975eb8ed4a15a9d99ec59a1680735c514e07a0a
Scott Minter <sminter@gmail.com>
2023-08-30 16:06:47 -0400

    added tests

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,5 @@ use App\Http\Controllers\HomeController;
 |
 */
 
-Route::get('', [HomeController::class, 'index'])->name('home');
+Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::post('/get-solution', [HomeController::class, 'getSolution']);
---
+++ tests/Feature/HomeControllerTest.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class HomeControllerTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     */
+    public function test_home(): void
+    {
+        $response = $this->get('/');
+        $response->assertStatus(200);
+    }
+
+    public function test_get_solution_post_addition(): void
+    {
+        $payload = [
+            '_token' => csrf_token(),
+            'number1' => 1,
+            'number2' => 2,
+            'mathType' => 'addition'
+        ];
+
+        $this->json('POST', '/get-solution', $payload)
+            ->assertStatus(302)
+            ->assertRedirect('/?number1=' . $payload['number1'] . '&number2=' . $payload['number2'] . '&math-type=' . $payload['mathType'] . '&solution=3');
+    }
+
+    public function test_get_solution_post_subtraction(): void
+    {
+        $payload = [
+            '_token' => csrf_token(),
+            'number1' => 10,
+            'number2' => 5,
+            'mathType' => 'subtraction'
+        ];
+
+        $this->json('POST', '/get-solution', $payload)
+            ->assertStatus(302)
+            ->assertRedirect('/?number1=' . $payload['number1'] . '&number2=' . $payload['number2'] . '&math-type=' . $payload['mathType'] . '&solution=5');
+    }
+
+    public function test_get_solution_post_multiplication(): void
+    {
+        $payload = [
+            '_token' => csrf_token(),
+            'number1' => 2,
+            'number2' => 3,
+            'mathType' => 'multiplication'
+        ];
+
+        $this->json('POST', '/get-solution', $payload)
+            ->assertStatus(302)
+            ->assertRedirect('/?number1=' . $payload['number1'] . '&number2=' . $payload['number2'] . '&math-type=' . $payload['mathType'] . '&solution=6');
+    }
+
+    public function test_get_solution_post_division(): void
+    {
+        $payload = [
+            '_token' => csrf_token(),
+            'number1' => 10,
+            'number2' => 2,
+            'mathType' => 'division'
+        ];
+
+        $this->json('POST', '/get-solution', $payload)
+            ->assertStatus(302)
+            ->assertRedirect('/?number1=' . $payload['number1'] . '&number2=' . $payload['number2'] . '&math-type=' . $payload['mathType'] . '&solution=5');
+    }
+
+    public function test_get_solution_post_division_by_zero(): void
+    {
+        $payload = [
+            '_token' => csrf_token(),
+            'number1' => 10,
+            'number2' => 0,
+            'mathType' => 'division'
+        ];
+
+        $this->json('POST', '/get-solution', $payload)
+            ->assertStatus(302)
+            ->assertRedirect('/?number1=' . $payload['number1'] . '&number2=' . $payload['number2'] . '&math-type=' . $payload['mathType'] . '&solution=0');
+    }
+}
